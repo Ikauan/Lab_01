08d25f4be13af182aa84949c8b3e964ebb2900e5
KAUAN MARTINS CARDOSO <kauan.mcardoso@senacsp.edu.br>
2022-04-14 17:18:43 -0300

    Adicionar arquivos de projeto.

---
+++ Lab_01/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿
+namespace Lab_01
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblCurso = new System.Windows.Forms.Label();
+            this.tbxNome = new System.Windows.Forms.TextBox();
+            this.cbxCurso = new System.Windows.Forms.ComboBox();
+            this.lblMenssagem = new System.Windows.Forms.Label();
+            this.btnExibir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblTitulo.Location = new System.Drawing.Point(91, 28);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(297, 25);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Visualizar dados na caixa de texto";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblNome.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.lblNome.Location = new System.Drawing.Point(91, 122);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(63, 25);
+            this.lblNome.TabIndex = 1;
+            this.lblNome.Text = "Nome";
+            this.lblNome.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // lblCurso
+            // 
+            this.lblCurso.AutoSize = true;
+            this.lblCurso.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblCurso.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblCurso.Location = new System.Drawing.Point(91, 173);
+            this.lblCurso.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblCurso.Name = "lblCurso";
+            this.lblCurso.Size = new System.Drawing.Size(61, 25);
+            this.lblCurso.TabIndex = 2;
+            this.lblCurso.Text = "Curso";
+            // 
+            // tbxNome
+            // 
+            this.tbxNome.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.tbxNome.Location = new System.Drawing.Point(160, 127);
+            this.tbxNome.Name = "tbxNome";
+            this.tbxNome.Size = new System.Drawing.Size(226, 23);
+            this.tbxNome.TabIndex = 3;
+            // 
+            // cbxCurso
+            // 
+            this.cbxCurso.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cbxCurso.FormattingEnabled = true;
+            this.cbxCurso.Items.AddRange(new object[] {
+            "Php",
+            "Python",
+            "Java",
+            "JavaScript",
+            "C#",
+            "C++",
+            "C"});
+            this.cbxCurso.Location = new System.Drawing.Point(160, 175);
+            this.cbxCurso.Name = "cbxCurso";
+            this.cbxCurso.Size = new System.Drawing.Size(98, 23);
+            this.cbxCurso.TabIndex = 5;
+            // 
+            // lblMenssagem
+            // 
+            this.lblMenssagem.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblMenssagem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblMenssagem.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblMenssagem.Location = new System.Drawing.Point(91, 270);
+            this.lblMenssagem.Name = "lblMenssagem";
+            this.lblMenssagem.Size = new System.Drawing.Size(352, 88);
+            this.lblMenssagem.TabIndex = 6;
+            this.lblMenssagem.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.lblMenssagem.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnExibir
+            // 
+            this.btnExibir.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.btnExibir.Location = new System.Drawing.Point(91, 217);
+            this.btnExibir.Name = "btnExibir";
+            this.btnExibir.Size = new System.Drawing.Size(352, 32);
+            this.btnExibir.TabIndex = 7;
+            this.btnExibir.Text = "Confirmar";
+            this.btnExibir.UseVisualStyleBackColor = false;
+            this.btnExibir.Click += new System.EventHandler(this.btnExibir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(782, 432);
+            this.Controls.Add(this.btnExibir);
+            this.Controls.Add(this.lblMenssagem);
+            this.Controls.Add(this.cbxCurso);
+            this.Controls.Add(this.tbxNome);
+            this.Controls.Add(this.lblCurso);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblCurso;
+        private System.Windows.Forms.TextBox tbxNome;
+        private System.Windows.Forms.ComboBox cbxCurso;
+        private System.Windows.Forms.Label lblMenssagem;
+        private System.Windows.Forms.Button btnExibir;
+    }
+}
+
